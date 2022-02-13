83fc4c0d1034f7d37c8458d1ec7e4edcde86541b
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-13 10:57:39 -0300

    add complexity comparisons

---
+++ src/dsa/intro_algo/complexity_comparisons.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+# Find lowest n for which first function runs faster than second function
+def find_equivalent_complexity(f1, f2):
+    complexities = [(n, f1(n), f2(n)) for n in np.arange(1, 50)]
+    lower_complexities = filter(lambda c: c[1] < c[2], complexities)
+    return next(lower_complexities)[0]-1
+
+
+# Examples
+find_equivalent_complexity(f1=lambda n: 100 * n ** 2,
+                           f2=lambda n: 2 ** n)
+# >>> 14
+
+find_equivalent_complexity(f1=lambda n: 8 * n ** 2,
+                           f2=lambda n: 64 * n * np.log(n))
+# >>> 1
+
