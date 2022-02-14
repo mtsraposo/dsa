47a392da39bfdcce8f40340b3812b33d5484e770
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-14 12:05:23 -0300

    add condition to filter out functions that explode too quickly or take too long to grow

--- src/dsa/intro_algo/complexity_table.py
+++ src/dsa/intro_algo/complexity_table.py
@@ -35,24 +35,34 @@ complexities = {'lg n': lambda n: np.log(n + 10 ** -12),
                 'n!': lambda n: math.factorial(n)}
 
 
+# Find first value for which the function exceeds a target value
 def minimize_naive(fun, t, n_threshold):
     eval_fun = [(n, fun(n)) for n in range(n_threshold)]
     return next(filter(lambda e: e[1] > t, eval_fun))[0]
 
 
-def find_upper_bound(fun, t, t_threshold=10 ** 16, n_threshold=30):
+def find_upper_bound(fun, t, tf_threshold=10 ** 16, nf_threshold=100, ns_threshold=10 ** 16):
     # To avoid calculating excessively large numbers,
-    # cap at max_n
-    if fun(100) > t_threshold:
-        return np.inf
+    # cap at max_n.
+    if fun(nf_threshold) > tf_threshold:
+        return nf_threshold
 
     # If the function explodes too quickly, proceed with naïve
     # minimization instead of finding inverse or optimizing.
-    if fun(n_threshold) > t:
-        return minimize_naive(fun, t, n_threshold)
+    if fun(nf_threshold) > t:
+        return minimize_naive(fun, t, nf_threshold)
+
+    # After eliminating functions that explode too quickly,
+    # filter out functions that are too slow to grow, and return
+    # infinity if they don't reach a threshold by a certain number
+    if fun(ns_threshold) < t:
+        return np.inf
 
     # Try to find explicit inverse function,
-    # otherwise, find an approximation
+    # otherwise, find an approximation.
+    # The inversefunc throws a warning when a certain precision hasn't been met.
+    # As we're looking for approximations here, they may be ignored.
+    warnings.filterwarnings('ignore')
     try:
         inv = inversefunc(fun)
         return inv(t)
@@ -71,15 +81,14 @@ def find_upper_bound(fun, t, t_threshold=10 ** 16, n_threshold=30):
         return math.floor(sol.x[0])
 
 
-warnings.filterwarnings('ignore')
-print('Time  |', '  |  '.join([f'{t:.2E}' for t in time_frames.values()]))
+max_name_len = max([len(s) for s in complexities.keys()])
+print(f"Time{' ' * max_name_len}|", '| '.join([f'{t:.2E} ' for t in time_frames.values()]))
 for c_name, c_fun in complexities.items():
-    print(c_name, end='')
+    print(f"{c_name}{' ' * (max_name_len - len(c_name) + 4)}", end='')
     for t in time_frames.values():
         problems_solved = find_upper_bound(fun=c_fun, t=t)
         if problems_solved == np.inf:
-            print(f' |    inf   ', end=' ')
+            print(f'|    inf   ', end='')
         else:
-            print(f' |  {problems_solved:.2E}', end=' ')
+            print(f'| {problems_solved:.2E} ', end='')
     print('\n')
-warnings.filterwarnings('defaul')
