cb7029a793f489b9cdf254b70e9af393792eda96
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-16 11:04:37 -0300

    add binary search

--- src/dsa/intro_algo/searching.py
+++ src/dsa/intro_algo/searching.py
@@ -5,6 +5,23 @@ def linear_search(arr, v):
     return None
 
 
+def binary_search(sorted_arr, v):
+    midpoint = len(sorted_arr) // 2
+    if v == sorted_arr[midpoint]:
+        return midpoint
+    else:
+        if len(sorted_arr) == 1:
+            return None
+        else:
+            if v > sorted_arr[midpoint]:
+                return midpoint + 1 + binary_search(sorted_arr[midpoint+1:], v)
+            else:
+                return binary_search(sorted_arr[:midpoint], v)
+
+
 if __name__ == '__main__':
     linear_search(arr=[5, 2, 4, 6, 1, 3],
                   v=7)
+
+    binary_search(sorted_arr=[1, 2, 3, 6, 9, 12, 15, 18, 21],
+                  v=18)
