650c3641f3aebeb2548d72be4a64f6b5476c970d
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-19 22:13:04 -0300

    include reflections to the central axis > magic square

--- src/dsa/hackerrank/magic_squares.py
+++ src/dsa/hackerrank/magic_squares.py
@@ -1,37 +1,60 @@
+def gen_rotations(elements):
+    return [elements[-i:] + elements[:-i] for i in range(len(elements))]
+
+
+def gen_all_rotations(elements, reindex):
+    rotations = gen_rotations(elements)
+    # add reflections relative to the central column
+    elements = [elements[i] for i in reindex]
+    rotations += gen_rotations(elements)
+    return rotations
+
+
+def calc_magic_rotation_cost(magic_rotation, rotations, elements):
+    r = rotations[magic_rotation]
+    return sum([abs(r[i] - elements[i]) for i in range(len(r))])
+
+
+def search_best_rotation(edges, edge_rotations, corners, corner_rotations):
+    total_cost = 100
+    magic_rotation = 0
+    for index, r in enumerate(edge_rotations):
+        edge_cost = sum([abs(r[i] - edges[i]) for i in range(len(r))])
+        corner_cost = calc_magic_rotation_cost(index, corner_rotations, corners)
+        print(edge_cost, corner_cost)
+        total_cost = min(edge_cost + corner_cost, total_cost)
+        print(total_cost)
+        magic_rotation = index if total_cost == edge_cost + corner_cost else magic_rotation
+    return total_cost, magic_rotation
+
+
 def forming_magic_square(s):
     """
-    There are only 4 possibilities to form a 3x3 magic square, and all of them
-    are rotations of the others and have a 5 at their centers.
+    There are only 8 possibilities to form a 3x3 magic square, and all of them
+    are rotations or reflections of the others and have a 5 at their centers.
     """
     center_cost = abs(s[1][1] - 5)  # Center cost may be calculated directly
 
-    # -- Search corner rotation that minimizes cost
-
-    corners = [s[0][0], s[0][2], s[2][2], s[2][0]]  # Clockwise corner values
-    magic_corners = [8, 4, 2, 6]  # Clockwise magic square corner values
-    magic_corner_rotations = [magic_corners[-i:] + magic_corners[:-i] for i in range(len(magic_corners))]
-
-    corner_cost = 36
-    magic_rotation = 2
-    for index, mc in enumerate(magic_corner_rotations):
-        cost = sum([abs(mc[i] - corners[i]) for i in range(len(mc))])
-        corner_cost = min(cost, corner_cost)
-        magic_rotation = index if cost == corner_cost else magic_rotation
-
-    # -- Given a magic_setting, calculate the cost to convert the edges
+    # -- Define corners and edges, as well as magic rotations
 
     edges = [s[0][1], s[1][2], s[2][1], s[1][0]]  # Clockwise edge values
     magic_edges = [3, 9, 7, 1]  # Clockwise magic square edge values
-    magic_edge_rotations = [magic_edges[-i:] + magic_edges[:-i] for i in range(len(magic_edges))]
-    me = magic_edge_rotations[magic_rotation]
-    edge_cost = sum([abs(me[i] - edges[i]) for i in range(len(me))])
+    magic_edge_rotations = gen_all_rotations(magic_edges, reindex=[0, 3, 2, 1])
 
-    return center_cost + corner_cost + edge_cost
+    corners = [s[0][0], s[0][2], s[2][2], s[2][0]]  # Clockwise corner values
+    magic_corners = [8, 4, 2, 6]  # Clockwise magic square corner values
+    magic_corner_rotations = gen_all_rotations(magic_corners, reindex=[1, 0, 3, 2])
+
+    # -- Search rotation that minimizes cost
+
+    edge_corner_cost = search_best_rotation(edges, magic_edge_rotations, corners, magic_corner_rotations)[0]
+
+    return center_cost + edge_corner_cost
 
 
 if __name__ == '__main__':
-    s = [[5, 3, 4],
-         [1, 5, 8],
-         [6, 4, 2]]
+    s = [[4, 5, 8],
+         [2, 4, 1],
+         [1, 9, 7]]
 
     print(forming_magic_square(s))
