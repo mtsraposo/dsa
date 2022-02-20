baf211da6cf19c8069318cc43012b1bf5a109bea
Matheus Raposo <matheus_raposo@hotmail.com>
2022-02-20 13:18:25 -0300

    add incentive to clean nearby cell

--- src/dsa/hackerrank/bot_clean.py
+++ src/dsa/hackerrank/bot_clean.py
@@ -20,36 +20,43 @@ def calc_distances_to_cells(posr, posc, d_cells):
     return sum([abs(posr - d[0]) + abs(posc - d[1]) for d in d_cells])
 
 
-def find_available_moves(posr, posc, moves):
-    return {k: v for k, v in moves.items() if posr + v[0] >= 0 and posc + v[1] >= 0}
+def find_available_moves(posr, posc, n):
+    return {k: v for k, v in ALL_MOVES.items() if (0 <= posr + v[0] < n and 0 <= posc + v[1] < n)}
 
 
 def calc_distances_after_moves(posr, posc, d_cells, moves):
     return {name: calc_distances_to_cells(*(posr + m[0], posc + m[1]), d_cells) for name, m in moves.items()}
 
 
-def find_move_to_lowest_distance(move_to_dist):
-    best_moves = [k for k, v in move_to_dist.items() if v == min(move_to_dist.values())]
-    if len(best_moves) > 1:
-        return best_moves[random.randint(0, len(best_moves)-1)]
-    return best_moves[0]
+def find_move_to_lowest_distance(posr, posc, move_to_dist, board):
+    moves_to_dirty = [m for m in move_to_dist.keys()
+                      if board[posr + ALL_MOVES[m][0]][posc + ALL_MOVES[m][1]] == 'd']
+    if len(moves_to_dirty) >= 1:
+        return moves_to_dirty[random.randint(0, len(moves_to_dirty) - 1)]
+    else:
+        moves = [k for k, v in move_to_dist.items() if v == min(move_to_dist.values())]
+        if len(moves) >= 1:
+            return moves[random.randint(0, len(moves) - 1)]
+        else:
+            return moves[0]
+
 
 
 def next_move(posr, posc, board):
     d_cells = find_all_dirty_cells(board)
-    if (posr, posc) in d_cells:
-        best_move = 'CLEAN'
+    if board[posr][posc] == 'd':
+        move = 'CLEAN'
     else:
-        moves = find_available_moves(posr, posc, ALL_MOVES)
+        moves = find_available_moves(posr, posc, len(board))
         move_to_dist = calc_distances_after_moves(posr, posc, d_cells, moves)
-        best_move = find_move_to_lowest_distance(move_to_dist)
-    print(best_move)
-    return best_move
+        move = find_move_to_lowest_distance(posr, posc, move_to_dist, board)
+    print(move)
+    return move
 
 
 def udpate_board(posr, posc, board, move):
     if move == 'CLEAN':
-        new_row = f'{board[posr][:posc]}-{board[posr][posc+1:]}'
+        new_row = f'{board[posr][:posc]}-{board[posr][posc + 1:]}'
         board[posr] = new_row
     else:
         posr, posc = posr + ALL_MOVES[move][0], posc + ALL_MOVES[move][1]
@@ -58,11 +65,11 @@ def udpate_board(posr, posc, board, move):
 
 if __name__ == '__main__':
     # -- Inputs --
-    s = """---d-
-           -bd--
-           -----
-           -----
-           d-d--"""
+    s = """bd---
+           -d---
+           ---d-
+           ---d-
+           --d-d"""
     s = re.sub('[ \t]*', '', s)
     board = []
     [board.append(r) for r in s.split('\n')]
@@ -75,3 +82,4 @@ if __name__ == '__main__':
         posr, posc, board = udpate_board(posr, posc, board, n_move)
         print(posr, posc, board)
         iter += 1
+    print(iter)
